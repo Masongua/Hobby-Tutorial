ce2e08e36f8e3303e3c23ac8975dfb042bd07fb9
Mason Guaman <masonstalin@gmail.com>
2022-10-06 13:06:13 -0400

    Initial Commit

---
+++ Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by 5e on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
